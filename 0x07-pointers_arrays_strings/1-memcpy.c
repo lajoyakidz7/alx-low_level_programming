56d16dd6177d11aab6584b216af872673f5a77b7
lajoyakidz7 <dstoffeltalifhani@gmail.com>
2023-07-04 08:02:24 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * simple_print_buffer - Prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ *  Return: nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{ 
+	unsigned int i:
+
+		i = 0;
+	while (i < size)
+	{
+		if ( i  % 10)
+		{
+			Printf(" ");
+		} 
+		if (!(i % 10) && I)
+		{
+			Printf("\n");
+		}
+		Printf("0x%02", buffer[i]);
+		i++
+	}
+return: (0)
+}
